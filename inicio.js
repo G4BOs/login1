d5347f846ace581bc15a1c45fcdcdf4f0ba7f3c0
G4BOs <108361351+G4BOs@users.noreply.github.com>
2023-12-15 14:38:43 -0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,9 @@
+const CERRARSESION = document.getElementById('cerrarsesion');
+
+
+CERRARSESION.addEventListener("click",cerrarsesion);
+
+function cerrarsesion(){
+    window.location.href='index.html';
+    return
+}
